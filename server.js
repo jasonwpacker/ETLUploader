2ea761b971370c89d8e06057189860ef9d6408b5
jasonwpacker <54761893+jasonwpacker@users.noreply.github.com>
2022-05-17 16:37:27 -0500

    Fresh Upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/upload_files", upload.array("files"), uploadFiles);
+
+function uploadFiles(req, res) {
+    console.log(req.body);
+    console.log(req.files);
+    res.json({ message: "Successfully Uploaded Files" });
+}
+
+app.listen(5000, () => {
+    console.log(`Server started...`);
+});
